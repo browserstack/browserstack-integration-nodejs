e5bed6dd219ff5d0c6f7eb184dadec4cb69c3207
Vibhaj Rajan <vibhaj8@gmail.com>
2016-10-29 21:31:18 +0530

    added local sample

---
+++ ruby/sample.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+@repeat = ENV['REPEAT'] || 5
+
+# Input capabilities
+driver = Selenium::WebDriver.for :firefox
+
+begin
+  driver.navigate.to "http://www.google.com"
+  element = driver.find_element(:name, 'q')
+  element.send_keys "BrowserStack"
+  element.submit
+  start = Time.now.to_i
+  @repeat.times do
+    puts driver.title
+  end
+  driver.quit
+rescue
+  driver.quit
+end
