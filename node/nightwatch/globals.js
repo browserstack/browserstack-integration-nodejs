48da6cd0e1bb14c7c2ea5d0019d74755f6bb5c46
Harish Ved <ved.harish3@gmail.com>
2016-11-09 18:02:15 +0530

    user after

--- node/nightwatch/globals.js
+++ node/nightwatch/globals.js
@@ -1,6 +1,10 @@
 module.exports = { 
   before: function(done) {
-    console.log('GLOBALS');
+    console.log('User-defined GLOBALS Before');
+    done();
+  },
+  after: function(done) {
+    console.log('User-defined GLOBALS After');
     done();
   }
 };
