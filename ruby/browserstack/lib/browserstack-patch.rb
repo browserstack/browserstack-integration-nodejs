4317a69c8499b9afc59b43941e4cc2c61197d59f
Harish Ved <ved.harish3@gmail.com>
2016-11-11 15:57:36 +0530

    random identifier

--- ruby/browserstack/lib/browserstack-patch.rb
+++ ruby/browserstack/lib/browserstack-patch.rb
@@ -40,7 +40,7 @@ end
 module BrowserStack
   @@framework = 'ruby'
   @@bs_local = nil
-  @@bstack_identifier = 'asd'
+  @@bstack_identifier = "bstack_patches_#{(0...16).map { ('a'..'z').to_a[rand(26)] }.join}"
 
   def self.for(framework)
     @@framework = framework
