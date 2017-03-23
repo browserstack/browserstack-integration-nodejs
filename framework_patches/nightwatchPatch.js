10d3f4647480b9d4af84599128096b463bb8822d
Igor Ognichenko <ognichenko.igor@gmail.com>
2017-03-23 10:20:59 +0200

    fix for 'gulp-nightwatch'

--- framework_patches/nightwatchPatch.js
+++ framework_patches/nightwatchPatch.js
@@ -1,29 +1,31 @@
-var path = require('path');
-
 exports.nightwatchPatch = function() {
   var patchOptions = {
     desiredCapabilities: {
       'browserstack.framework': 'nightwatch'
     }
   };
+
   return {
     addCapability: function(key, value) {
       patchOptions['desiredCapabilities'][key] = value;
     },
+
     seleniumHost: function(host, port) {
       patchOptions['seleniumHost'] = host;
       patchOptions['seleniumPort'] = port;
     },
+
     trackFrameworkVersion: function() {
-      var nightwatchPackage = require(path.join(require.main.filename, '../../package.json'));
+      var nightwatchPackage = require(patchOptions.desiredCapabilities['browserstack.framework'] + '/package.json');
       this.addCapability('browserstack.framework_version', nightwatchPackage.version);
     },
-    patch: function(beforeAll, afterAll) {
-      var nightwatch = require(path.join(require.main.filename, '../../lib/index.js'));
-      var client = nightwatch.client;
 
-      var CliRunner = require(path.join(require.main.filename, '../../lib/runner/cli/clirunner.js'));
+    patch: function(beforeAll, afterAll) {
+      var nightwatch = require(patchOptions.desiredCapabilities['browserstack.framework'] + '/lib/index.js');
+      var client = nightwatch.client;
+      var CliRunner = require(patchOptions.desiredCapabilities['browserstack.framework'] + '/lib/runner/cli/clirunner.js');
       var origStartSelenium = CliRunner.prototype.startSelenium;
+
       CliRunner.prototype.startSelenium = function(callback) {
         var self = this;
         beforeAll(function() {
@@ -35,11 +37,13 @@ exports.nightwatchPatch = function() {
         Object.keys(patchOptions.desiredCapabilities).forEach(function(patchKey) {
           options.desiredCapabilities[patchKey] = patchOptions.desiredCapabilities[patchKey];
         });
+
         Object.keys(patchOptions).forEach(function(patchKey) {
           if(patchKey != 'desiredCapabilities') {
             options[patchKey] = patchOptions[patchKey];
           }
         });
+
         options.globals = options.globals || {};
         var origAfter = options.globals.after;
         origAfter = origAfter || function(done) { done(); };
@@ -48,6 +52,7 @@ exports.nightwatchPatch = function() {
             origAfter(done);
           });
         };
+
         return client(options);
       };
     }
